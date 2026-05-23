11881e8734e0efb1ae8570f2101ada1b05688714
monoyuh <monoyuh@users.noreply.github.com>
2026-05-23 15:21:22 +0000

    fix: use GitHub mirror for xz download (tukaani.org unreliable)

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -16,7 +16,7 @@ fetch_source "https://github.com/PhialsBasement/runit/releases/download/2.2.0/ru
 fetch_source "https://github.com/facebook/zstd/releases/download/v$ZSTD_VERSION/zstd-$ZSTD_VERSION.tar.gz"
 fetch_source "https://zlib.net/fossils/zlib-$ZLIB_VERSION.tar.gz"
 fetch_source "https://sourceware.org/pub/bzip2/bzip2-$BZIP2_VERSION.tar.gz"
-fetch_source "https://tukaani.org/xz/xz-$XZ_VERSION.tar.gz"
+fetch_source "https://github.com/tukaani-project/xz/releases/download/v$XZ_VERSION/xz-$XZ_VERSION.tar.gz"
 fetch_source "https://libarchive.org/downloads/libarchive-$LIBARCHIVE_VERSION.tar.xz"
 fetch_source "https://openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz"
 fetch_source "https://curl.se/download/curl-$CURL_VERSION.tar.xz"
